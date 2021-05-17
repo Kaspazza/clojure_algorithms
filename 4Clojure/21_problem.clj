6003c62ddb95bea31afabe04c7c4b2b8f360f1bc
mateusz.mazurczak <kaspazza@gmail.com>
2021-05-17 14:57:30 +0200

    Add solution to 4clojure problem 21

---
+++ 4Clojure/21_problem.clj
@@ -0,0 +1,10 @@
+;; Nth Element
+;; https://www.4clojure.com/problem/21
+
+(= ((fn [col step] (if (= step 0) (first col) (recur (rest col) (- step 1)))) '(4 5 6 7) 2) 6)
+
+(= (#(first (drop %2 %)) [:a :b :c] 0) :a)
+
+(= (#(->> % (drop %2) first) [1 2 3 4] 1) 2)
+
+(= ((fn [col step] (first (drop step col))) '([1 2] [3 4] [5 6]) 2) [5 6])
